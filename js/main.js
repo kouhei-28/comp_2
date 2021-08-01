45295c08d468a0ffd365366b4d7d2e260e7aea1c
kouhei-28 <64849963+kouhei-28@users.noreply.github.com>
2021-08-01 17:42:16 +0900

    swiper

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 'use strict';
 
 {
-
+  let mySwiper = new Swiper(".swiper-container", {
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+  });
 }
