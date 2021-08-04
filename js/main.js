c8355e2ab6f1a9ea87835ad0ba0997bc9c8bae1e
kouhei-28 <64849963+kouhei-28@users.noreply.github.com>
2021-08-04 21:43:32 +0900

    ハンバーガーメニュー実装

--- js/main.js
+++ js/main.js
@@ -34,4 +34,20 @@
   const observer = new IntersectionObserver(callback, options);
 
   observer.observe(target);
+
+  const button = document.querySelector(".toggle_btn");
+  const aside = document.querySelector("aside");
+  const links = document.querySelectorAll(".sp-ul > li > a");
+
+  button.addEventListener("click", () => {
+    aside.classList.toggle("open");
+    button.classList.toggle("close");
+  });
+
+  links.forEach((link) => {
+    link.addEventListener("click", () => {
+      aside.classList.toggle("open");
+      button.classList.toggle("close");
+    });
+  });
 }
